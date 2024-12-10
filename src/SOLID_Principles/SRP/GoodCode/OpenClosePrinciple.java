3bf4050778dcb98b95a3b51b04fd0abdeccc69df
prashant <prashanttiwary10@gmail.com>
2024-12-10 14:02:05 +0530

    SOLID

--- src/SOLID_Principles/SRP/GoodCode/OpenClosePrinciple.java
+++ src/SOLID_Principles/SRP/GoodCode/OpenClosePrinciple.java
@@ -4,45 +4,49 @@ package SOLID_Principles.SRP.GoodCode;
 
 //Now if we have to add a new payment method, we can add it without making any modification
 interface PaymentMethod {
-    public void processPayment(double amount);
+    public void pay(double amount);
 }
 
 class CreditCard implements PaymentMethod {
     @Override
-    public void processPayment(double amount) {
+    public void pay(double amount) {
         System.out.println("Credit Card Payment amount: " + amount);
     }
 }
 
 class DebitCard implements PaymentMethod {
     @Override
-    public void processPayment(double amount) {
+    public void pay(double amount) {
         System.out.println("Debit Card Payment amount: " + amount);
     }
 }
 
 class Cash implements PaymentMethod {
     @Override
-    public void processPayment(double amount) {
+    public void pay(double amount) {
         System.out.println("Cash Payment amount: " + amount);
     }
 }
-
 
 //added new payment method without making changes to existing code
 //open for extension but closed for modification
 class UPI implements PaymentMethod {
     @Override
-    public void processPayment(double amount) {
+    public void pay(double amount) {
         System.out.println("UPI Payment amount: " + amount);
     }
 }
 
+class PaymentProcessor {
+    public void processPayment(PaymentMethod paymentMethod,double amount) {
+        paymentMethod.pay(amount);
+    }
+}
+
+
 public class OpenClosePrinciple {
     public static void main(String[] args) {
-        PaymentMethod paymentMethod = new CreditCard();
-        paymentMethod.processPayment(10);
-        PaymentMethod paymentMethod2 = new UPI();
-        paymentMethod2.processPayment(10);
+       PaymentProcessor paymentProcessor = new PaymentProcessor();
+       paymentProcessor.processPayment(new UPI(),10);
     }
 }
