e13409d3cfe090fffdac3e1e449899725f27df48
prashant <prashanttiwary10@gmail.com>
2024-12-10 17:28:51 +0530

    SOLID

---
+++ src/SOLID_Principles/SRP/BadCode/LiskovSubstitutionPrincipleViolation.java
@@ -0,0 +1,7 @@
+package SOLID_Principles.SRP.BadCode;
+
+//Object of the super class is replaceable with an object of the subclass class
+//Ensures that child class can stand in for its parent class and function correctly in any context that expects parent class.
+
+public class LiskovSubstitutionPrincipleViolation {
+}
