5081aff269bc6a4b318d2ad8d164ea69e3d7b88a
prashant <prashanttiwary10@gmail.com>
2024-12-13 22:15:20 +0530

    SOLID

--- src/SOLID_Principles/SRP/BadCode/LiskovSubstitutionPrincipleViolation.java
+++ src/SOLID_Principles/SRP/BadCode/LiskovSubstitutionPrincipleViolation.java
@@ -3,5 +3,40 @@ package SOLID_Principles.SRP.BadCode;
 //Object of the super class is replaceable with an object of the subclass class
 //Ensures that child class can stand in for its parent class and function correctly in any context that expects parent class.
 
+class File {
+
+    public void read() {
+        System.out.println("Reading file");
+    }
+
+    public void write() {
+        System.out.println("writing file");
+    }
+}
+
+class ReadOnly extends File {
+
+    @Override
+    public void read() {
+        System.out.println("Reading read-only file");
+    }
+
+    //This is specific to read-only we cannot use write operation
+    @Override
+    public void write() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
+
 public class LiskovSubstitutionPrincipleViolation {
+    public static void main(String[] args) {
+        File file = new File();
+        file.read(); // works fine
+        file.write(); // works fine
+
+        //now let's use a child object
+        file = new ReadOnly();
+        file.read(); // works fine
+        file.write();// exception will come violation of lsp
+    }
 }
