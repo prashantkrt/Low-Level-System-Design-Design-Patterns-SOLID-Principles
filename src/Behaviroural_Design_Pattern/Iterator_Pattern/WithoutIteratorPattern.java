076a0f9c81416fd395e2236ec7bef5d1a07c7c5a
prashant <prashanttiwary10@gmail.com>
2024-12-16 23:40:59 +0530

    Iterator Pattern

---
+++ src/Behaviroural_Design_Pattern/Iterator_Pattern/WithoutIteratorPattern.java
@@ -0,0 +1,59 @@
+package Behaviroural_Design_Pattern.Iterator_Pattern;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Book{
+
+     private String title;
+
+     public Book(String title){
+         this.title = title;
+     }
+
+     public String getTitle(){
+         return title;
+     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                '}';
+    }
+}
+
+class BookCollection{
+
+    //what if I wanted to make this List to Set
+     private List<Book> books = new ArrayList<Book>();
+
+     //have to make changes for iterating over the set interface
+    // private Set<Book> books = new HashSet<>();
+
+     public void addBook(Book book){
+         books.add(book);
+     }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+}
+
+
+public class WithoutIteratorPattern {
+    public static void main(String[] args) {
+        BookCollection bookCollection = new BookCollection();
+        bookCollection.addBook(new Book("Book 1"));
+        bookCollection.addBook(new Book("Book 2"));
+        bookCollection.addBook(new Book("Book 3"));
+
+        //suppose I have changed the List to Set in the class BookCollection in that case getT
+        for(Book book : bookCollection.getBooks()){
+            System.out.println(book.getTitle());
+        }
+
+    }
+}
